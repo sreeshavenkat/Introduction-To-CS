b7ea2f3a0d7bd6a04a410d5c25ca769059760446
Sreesha Venkat <sreeshavenkat@gmail.com>
2019-06-23 11:31:49 -0400

    homework 5

---
+++ hw5.py
@@ -0,0 +1,39 @@
+def is_prime(x):
+    """
+    Return True if x is a prime number, otherwise return False
+    Hint: Look up what the modulo operator does in Python and try to use it here
+
+    >>> is_prime(7)
+    True
+    >>> is_prime(8)
+    False
+    """
+    "YOUR CODE HERE"
+
+def sum_of_squares(n):
+    """
+    Return the sum of squares for numbers from 1...n (inclusive)
+
+    >>> sum_of_squares(4) # 1*1 + 2*2 + 3*3 + 4*4
+    30
+    """
+    "*** YOUR CODE HERE ***"
+
+def fizz_buzz(n):
+    """
+    Write a function that prints numbers 1...n (inclusive). However, if the number is divisible
+    by 3 print "fizz", if the number is divisible by 2 print "buzz", 
+    or if the number is divisible by both print "fizzbuzz"
+
+    >>> fizz_buzz(7)
+    1
+    buzz
+    fizz
+    buzz
+    5
+    fizzbuzz
+    7
+    """
+    "*** YOUR CODE HERE ***"
+
+
