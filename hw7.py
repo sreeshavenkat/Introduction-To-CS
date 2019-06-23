7351aeba7e0ed993f67466a3e1d455a6bc923ae1
Sreesha Venkat <sreeshavenkat@gmail.com>
2019-06-23 11:46:51 -0400

    homework 7

---
+++ hw7.py
@@ -0,0 +1,47 @@
+# NOTE: Do not use any iterative methods (for/while loops to solve these problems)
+
+def factorial(n):
+	"""
+	Return the factorial of a positive integer n
+	
+	>>> factorial(7) # 7 * 6 * 5 * 4 * 3 * 2 * 1
+    5040
+	"""
+	"YOUR CODE HERE"
+
+def is_prime(n):
+	"""
+	Return True if x is a prime number, otherwise return False
+    Hint: Look up what the modulo operator does in Python and try to use it here
+
+    >>> is_prime(7)
+    True
+    >>> is_prime(8)
+    False
+    """
+	"YOUR CODE HERE"
+
+def fibonacci(n):
+	"""
+	Return the nth number in the fibonacci sequence
+
+	>>> fibonacci(6)
+	5
+	>>> fibonacci(9)
+	21
+	"""
+	"YOUR CODE HERE"
+
+def descending(n):
+	"""
+	Print out a descending countdown of integers from n to 1
+
+	>>> descending(6)
+	6
+	5
+	4
+	3
+	2
+	1
+	"""
+	"YOUR CODE HERE"
