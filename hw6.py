b5256a52872b782e6dfeced7fdbad3cb8f1ad712
Sreesha Venkat <sreeshavenkat@gmail.com>
2019-06-23 11:58:24 -0400

    homework 6

---
+++ hw6.py
@@ -0,0 +1,33 @@
+def has_five(k):
+    """
+    Return True if at least one of the digits of k is a 5, False otherwise.
+
+    >>> has_five(3)
+    False
+    >>> has_five(5)
+    True
+    >>> has_five(2734)
+    False
+    >>> has_five(2534)
+    True
+    >>> has_five(734)
+    False
+    >>> has_five(555)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+def is_palindrome(n):
+    """
+    Return True if n is a palindrome, False otherwise
+    Note: A palindrome is a number that reads the same if you reverse it.
+    >>> is_palindrome(2928)
+    False
+    >>> is_palindrome(52325)
+    True
+    >>> is_palindrome(2)
+    True
+    >>> is_palindrome(444)
+    True
+    """
+    "*** YOUR CODE HERE ***"
