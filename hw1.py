fec902a5f938eb91c6f68df11c58fd2c6ba53d94
Sreesha Venkat <sreeshavenkat@gmail.com>
2019-06-23 09:08:04 -0400

    homework 1

---
+++ hw1.py
@@ -0,0 +1,45 @@
+from operator import add, sub
+
+def a_minus_abs_b(a, b):
+    """
+    Return a - abs(b) without calling the abs function.
+
+    >>> a_minus_abs_b(2, 3)
+    -1
+    >>> a_minus_abs_b(2, -3)
+    -1
+    """
+    if b < 0:
+        f = _____
+    else:
+        f = _____
+    return f(a, b)
+
+def two_of_three(a, b, c):
+    """
+    Return x*x + y*y, where x and y are the two largest members of the
+    positive numbers a, b, and c.
+
+    >>> two_of_three(1, 2, 3)
+    13
+    >>> two_of_three(5, 3, 1)
+    34
+    >>> two_of_three(10, 2, 8)
+    164
+    >>> two_of_three(5, 5, 5)
+    50
+    """
+    "*** YOUR CODE HERE ***"
+
+def largest_factor(n):
+    """
+    Return the largest factor of n.
+
+    >>> largest_factor(15) # factors are 1, 3, 5
+    5
+    >>> largest_factor(80) # factors are 1, 2, 4, 5, 8, 10, 16, 20, 40
+    40
+    """
+    "*** YOUR CODE HERE ***"
+
+
