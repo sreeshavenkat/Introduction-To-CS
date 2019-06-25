d423996a10a1cccb1d9472a86bea368f28fb42e1
Sreesha Venkat <sreeshavenkat@gmail.com>
2019-06-25 07:18:45 -0700

    Update hw7.py

--- hw7.py
+++ hw7.py
@@ -1,17 +1,17 @@
 # NOTE: Do not use any iterative methods (for/while loops to solve these problems)
 
 def factorial(n):
-	"""
-	Return the factorial of a positive integer n
-	
-	>>> factorial(7) # 7 * 6 * 5 * 4 * 3 * 2 * 1
+    """
+    Return the factorial of a positive integer n
+    
+    >>> factorial(7) # 7 * 6 * 5 * 4 * 3 * 2 * 1
     5040
-	"""
-	"YOUR CODE HERE"
+    """
+    "YOUR CODE HERE"
 
 def is_prime(n):
-	"""
-	Return True if x is a prime number, otherwise return False
+    """
+    Return True if x is a prime number, otherwise return False
     Hint: Look up what the modulo operator does in Python and try to use it here
 
     >>> is_prime(7)
@@ -19,29 +19,29 @@ def is_prime(n):
     >>> is_prime(8)
     False
     """
-	"YOUR CODE HERE"
+    "YOUR CODE HERE"
 
 def fibonacci(n):
-	"""
-	Return the nth number in the fibonacci sequence
-
-	>>> fibonacci(6)
-	5
-	>>> fibonacci(9)
-	21
-	"""
-	"YOUR CODE HERE"
+    """
+    Return the nth number in the fibonacci sequence
+    
+    >>> fibonacci(6)
+    5
+    >>> fibonacci(9)
+    21
+    """
+    "YOUR CODE HERE"
 
 def descending(n):
-	"""
-	Print out a descending countdown of integers from n to 1
-
-	>>> descending(6)
-	6
-	5
-	4
-	3
-	2
-	1
-	"""
-	"YOUR CODE HERE"
+    """
+    Print out a descending countdown of integers from n to 1
+    
+    >>> descending(6)
+    6
+    5
+    4
+    3
+    2
+    1
+    """
+    "YOUR CODE HERE"
